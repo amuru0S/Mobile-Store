b843142bb61dc9e2a878e9b0711f68308b4fe281
amuru0S <amuru.codais@gmail.com>
2019-05-02 13:15:20 +0200

    created-product-page-and-details-page

---
+++ src/components/Cart.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Cart extends Component {
+  render() {
+    return (
+      <div>
+        <h5> This is the Cart page </h5>
+      </div>
+    )
+  }
+}
---
+++ src/components/Default.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Default extends Component {
+  render() {
+    return (
+      <div>
+        <h5> Page doesnt exists </h5>
+      </div>
+    )
+  }
+}
